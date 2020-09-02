aadb9f8d0c169f0593d81db5beea406e4d071908
seonglae <sungle3737@gmail.com>
2020-09-02 13:15:11 +0900

    no colsole

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -1,8 +1,6 @@
 const beforeList = ["index", "signup"]
 
 export default function ({ store, route, _, redirect }) {
-  if (process.server) return
-  console.log(store.state, route.name)
   if (
     !store.state.localStorage.accessToken &&
     route.name !== "index" &&
