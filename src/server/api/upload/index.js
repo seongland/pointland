95e2d933e43a67a6247dd257338e167bfb7bca85
seonglae <sungle3737@gmail.com>
2020-09-15 13:35:49 +0900

    divide 500 upload

--- src/server/api/upload/index.js
+++ src/server/api/upload/index.js
@@ -2,7 +2,7 @@ import { PythonShell } from 'python-shell'
 import express from 'express'
 import dotenv from 'dotenv'
 
-const DIV_FACTOR = 1000
+const DIV_FACTOR = 500
 
 dotenv.config()
 const router = express.Router()
@@ -23,28 +23,30 @@ function upload(req, res) {
     console.log("date, maker, snap, schema", body.date, body.maker, body.snap, body.schema)
     const divCount = Math.ceil(length / DIV_FACTOR)
     const tempArray = new Array(divCount).fill(0)
+    const results = []
+    console.log(`Group Total: ${tempArray.length}`)
     for (const count in tempArray) {
+      const tempData = body.data.slice(count * DIV_FACTOR, (Number(count) + 1) * DIV_FACTOR)
+      console.log(`Group ${count} add started with count ${tempData.length}`)
       pythonOptions.args = [
-        JSON.stringify(body.data.slice(count * 1000, (count + 1) * 1000)),
+        JSON.stringify(tempData),
         JSON.stringify(body.add),
         JSON.stringify(body.date),
         JSON.stringify(body.maker),
         JSON.stringify(body.snap),
         JSON.stringify(body.schema)
       ]
-      PythonShell.run('upload/pg_uploader.py', pythonOptions, (err, results) => {
+      PythonShell.run('upload/pg_uploader.py', pythonOptions, (err, result) => {
         if (err) console.log(`Upload err in ${count} is `, err)
         if (err) res.json({ count, err, results })
         else {
-          console.log("Complete with No Error!")
-          res.json({ results })
+          results.push(result)
+          console.log(`Complete ${count} with No Error!`)
+          if (Number(count) === tempArray.length - 1) res.json({ results })
         }
       })
     }
   }
-
-
-
 }
 
 export default router
