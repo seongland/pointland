1d45bedea515aa2e0686d118ca3b6a5e9bd3c906
seonglae <sungle3737@gmail.com>
2020-09-15 11:24:55 +0900

    ✌ping flag refactoring

--- src/server/api/upload/index.js
+++ src/server/api/upload/index.js
@@ -2,6 +2,8 @@ import { PythonShell } from 'python-shell'
 import express from 'express'
 import dotenv from 'dotenv'
 
+const DIV_FACTOR = 1000
+
 dotenv.config()
 const router = express.Router()
 
@@ -14,25 +16,35 @@ const pythonOptions = {
 router.post('/', upload)
 
 function upload(req, res) {
-  if (req.body.data) console.log("Count is ", req.body.data.length)
   const body = req.body
-  console.log("date, maker, snap, schema", body.date, body.maker, body.snap, body.schema)
-  pythonOptions.args = [
-    JSON.stringify(req.body.data),
-    JSON.stringify(req.body.add),
-    JSON.stringify(req.body.date),
-    JSON.stringify(req.body.maker),
-    JSON.stringify(req.body.snap),
-    JSON.stringify(req.body.schema)
-  ]
-  PythonShell.run('upload/pg_uploader.py', pythonOptions, (err, results) => {
-    if (err) console.log("Upload err is ", err)
-    if (err) res.json({ err, results })
-    else {
-      console.log("Complete with No Error!")
-      res.json({ results })
+  if (body.data) {
+    const length = body.data.length
+    console.log("Count is ", length)
+    console.log("date, maker, snap, schema", body.date, body.maker, body.snap, body.schema)
+    const divCount = Math.ceil(length / DIV_FACTOR)
+    const tempArray = new Array(divCount).fill(0)
+    for (const count in tempArray) {
+      pythonOptions.args = [
+        JSON.stringify(body.data.slice(count * 1000, (count + 1) * 1000)),
+        JSON.stringify(body.add),
+        JSON.stringify(body.date),
+        JSON.stringify(body.maker),
+        JSON.stringify(body.snap),
+        JSON.stringify(body.schema)
+      ]
+      PythonShell.run('upload/pg_uploader.py', pythonOptions, (err, results) => {
+        if (err) console.log(`Upload err in ${count} is `, err)
+        if (err) res.json({ count, err, results })
+        else {
+          console.log("Complete with No Error!")
+          res.json({ results })
+        }
+      })
     }
-  })
+  }
+
+
+
 }
 
 export default router
