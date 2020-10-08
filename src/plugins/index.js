6d6328320a6195bfc160fab13acda5bd5b8ce803
seonglae <sungle3737@gmail.com>
2020-10-08 11:45:58 +0900

    change to set

--- src/plugins/index.js
+++ src/plugins/index.js
@@ -1,62 +1,50 @@
 import Vue from 'vue'
 import { olInit } from '~/plugins/map/meta'
-import { drawXYs, drawXY, subtractVhcl } from "./map/draw"
-import { initCloud, purgeCloud } from "./cloud/meta"
-import { drawLas } from "./cloud/draw"
+import { drawXYs, drawXY, subtractVhcl } from './map/draw'
+import { initCloud, purgeCloud } from './cloud/meta'
+import { drawLas } from './cloud/draw'
 import AsyncComputed from 'vue-async-computed'
 
 Vue.use(AsyncComputed)
 
-export default ({ $axios }) => {
+export default ({ $axios, store: { commit } }) => {
+  if (process.client) {
+    const origin = window.location.origin
+    $axios.defaults.baseURL = origin
+  }
+
   Vue.mixin({
     data: () => ({
       title: '3D MAPPING',
       coor: 'Stryx',
-      meta: {
-        version: undefined
-      },
+      meta: { version: undefined },
       map: undefined,
       tabs: [
-        {
-          name: 'Map',
-          type: 'map'
-        },
-        {
-          name: 'Image',
-          type: 'image'
-        },
-        {
-          name: '3D',
-          type: '3d'
-        }
+        { name: 'Map', type: 'map' },
+        { name: 'Image', type: 'image' },
+        { name: '3D', type: '3d' }
       ]
     }),
+
     methods: {
       initCloud: () => initCloud(),
       purgeCloud: () => purgeCloud(),
-      drawLas: (lasJson) => drawLas(lasJson),
+      drawLas: lasJson => drawLas(lasJson),
       drawXYs: (latlngs, id) => drawXYs(latlngs, id),
-      subtractVhcl: (id) => subtractVhcl(id),
+      subtractVhcl: id => subtractVhcl(id),
+      setRound: round => commit('ls/setRound', round),
+      setSnap: snap => commit('ls/setSnap', snap),
+      setSeq: seq => commit('ls/setSeq', seq),
       drawXY: (latlng, focus, id) => drawXY(latlng, focus, id),
 
-      changeRound(round) {
-        this.$store.commit('ls/changeRound', round)
-      },
-      changeSnap(snap) {
-        this.$store.commit('ls/changeSnap', snap)
-      },
-      changeSeq(seq) {
-        this.$store.commit('ls/changeSeq', seq)
+      setLayer(layer) {
+        console.log(layer)
       },
 
       async loadProjects(user, accessToken) {
         const projectPromises = []
         const config = { headers: { Authorization: accessToken } }
-        for (const i in user.projects)
-          projectPromises.push(this.$axios.get(
-            `/api/projects?id=${user.projects[i].id}`,
-            config
-          ))
+        for (const i in user.projects) projectPromises.push($axios.get(`/api/projects?id=${user.projects[i].id}`, config))
         const projectResponses = await Promise.all(projectPromises)
         user.projects = projectResponses.map(res => res.data[0])
       },
@@ -67,14 +55,8 @@ export default ({ $axios }) => {
       },
 
       waitAvail(flag, callback, args) {
-        this.$nextTick(
-          () => flag() ? callback(...args) : this.waitAvail(flag, callback, args)
-        )
+        this.$nextTick(() => (flag() ? callback(...args) : this.waitAvail(flag, callback, args)))
       }
     }
   })
-  if (process.client) {
-    const origin = window.location.origin
-    $axios.defaults.baseURL = origin
-  }
 }
