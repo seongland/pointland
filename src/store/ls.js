1ce7ee1c2ec08bae54a602419a6a233da0b3f0b9
seonglae <sungle3737@gmail.com>
2020-10-08 12:01:03 +0900

    format error modification

--- src/store/ls.js
+++ src/store/ls.js
@@ -9,88 +9,63 @@ export const state = () => ({
   currentRound: {
     name: 'imms_20200825_170217',
     snaps: [
-      {
-        name: 'snap1',
-        count: 1017
-      },
-      {
-        name: 'snap2',
-        count: 1017
-      }
+      { name: 'snap1', count: 1017 },
+      { name: 'snap2', count: 1017 }
     ]
   },
-  currentSnap: {
-    name: 'snap1',
-    count: 1017
-  },
+  currentSnap: { name: 'snap1', count: 1017 },
   currentSeq: 0,
   rounds: [
     {
       name: 'imms_20200825_170217',
       snaps: [
-        {
-          name: 'snap1',
-          count: 1017
-        },
-        {
-          name: 'snap2',
-          count: 1017
-        }
+        { name: 'snap1', count: 1017 },
+        { name: 'snap2', count: 1017 }
       ]
     },
     {
       name: 'imms_20200824_193802',
       snaps: [
-        {
-          name: 'snap1',
-          count: 1017
-        },
-        {
-          name: 'snap2',
-          count: 1017
-        }
+        { name: 'snap1', count: 1017 },
+        { name: 'snap2', count: 1017 }
       ]
     }
   ]
 })
 
 export const mutations = {
-         login(state, { accessToken, user }) {
-           state.user = user
-           state.accessToken = accessToken
-         },
+  login(state, { accessToken, user }) {
+    state.user = user
+    state.accessToken = accessToken
+  },
 
-         logout(state) {
-           /**
-            * @summary - Reset Token & Last Page
-            */
-           state.accessToken = undefined
-           state.user = undefined
-           state.prj = undefined
-           state.prjId = undefined
-           state.index = 0
-           state.currentSeq = 0
-           this.$router.push('/')
-         },
+  logout(state) {
+    /**
+     * @summary - Reset Token & Last Page
+     */
+    state.accessToken = undefined
+    state.user = undefined
+    state.prj = undefined
+    state.prjId = undefined
+    state.index = 0
+    state.currentSeq = 0
+    this.$router.push('/')
+  },
 
-         setIndex(state, index) {
-           /**
-            * @summary - change tab  & resize because of canvas error
-            */
-           state.index = index
-           if (index !== 1) setTimeout(() => window.dispatchEvent(new Event('resize')))
-           if (index === 2) setTimeout(() => updateCtrl())
-         },
+  setIndex(state, index) {
+    /**
+     * @summary - change tab  & resize because of canvas error
+     */
+    state.index = index
+    if (index !== 1) setTimeout(() => window.dispatchEvent(new Event('resize')))
+    if (index === 2) setTimeout(() => updateCtrl())
+  },
 
-         setRound(state, round) {
-           state.currentRound = round
-         },
-         setSnap(state, snap) {
-           state.currentSnap = snap
-         },
-         setSeq(state, seq) {
-           if (seq < 0) return
-           if (seq >= state.currentSnap.count) return
-           state.currentSeq = seq
-         }
-       }
+  setRound: (state, round) => (state.currentRound = round),
+  setSnap: (state, snap) => (state.currentSnap = snap),
+  setSeq(state, seq) {
+    if (seq < 0) return
+    if (seq >= state.currentSnap.count) return
+    state.currentSeq = seq
+  }
+}
