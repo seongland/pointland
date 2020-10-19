dbdb23641a4a25dcabfa396ed9b91ad5ba33ea5d
seonglae <sungle3737@gmail.com>
2020-10-19 10:11:53 +0900

    0.9.0

--- src/components/overlay/InputData.vue
+++ src/components/overlay/InputData.vue
@@ -2,19 +2,31 @@
   <v-row align="center" justify="center">
     <v-col lg="6" md="8" sm="12">
       <v-card class="elevation-24">
-        <v-card-subtitle class="unselectable pb-0"> Config </v-card-subtitle>
+        <v-card-title v-text="layerObj.description" />
+        <v-divider />
+        <v-card-title> Geometry </v-card-title>
+        <v-card-text v-text="`X : ${selected[0].properties.x}`" />
+        <v-card-text v-text="`Y : ${selected[0].properties.y}`" />
+        <v-card-text v-text="`Z : ${selected[0].properties.z}`" />
+        <v-card-text v-text="`Longitude : ${selected[0].geometry.coordinates[0]}`" />
+        <v-card-text v-text="`Latitude : ${selected[0].geometry.coordinates[1]}`" />
+
+        <v-divider />
+        <v-card-title v-text="`Image`" />
+        <v-card-text v-text="`Round : ${ls.currentRound.name}`" />
+        <v-card-text v-text="`Snap : ${ls.currentSnap.name}`" />
+        <v-card-text v-text="`Mark : ${ls.currentMark.name}`" />
+        <v-card-text v-text="`X : ${relations.images[0].coordinates[0]}`" />
+        <v-card-text v-text="`Y : ${relations.images[0].coordinates[1]}`" />
+        <v-divider />
+
+        <v-card-title> Comment </v-card-title>
         <v-card-text>
-          <v-text-field label="Argument 1" type="text"></v-text-field>
-
-          <v-text-field label="Argument 2" name="password" type="password"></v-text-field>
-
-          <v-text-field label="Argument 3" type="text"></v-text-field>
-
-          <v-text-field label="Argument 4" name="password" type="password"></v-text-field>
+          <v-text-field label="Comment" v-model="comment" />
         </v-card-text>
         <v-card-actions>
           <v-spacer></v-spacer>
-          <v-btn @click="$store.dispatch('')">Submit</v-btn>
+          <v-btn @click="submit">Submit</v-btn>
         </v-card-actions>
       </v-card>
     </v-col>
@@ -22,7 +34,35 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({ comment: undefined }),
+  computed: {
+    relations() {
+      return this.$store.state.selected[0].relations
+    },
+    layerObj() {
+      return this.$store.state.targetLayer.object
+    },
+    ls() {
+      return this.$store.state.ls
+    },
+    selected() {
+      return this.$store.state.selected
+    }
+  },
+  methods: {
+    async submit() {
+      const commit = this.$store.commit
+      await commit('submit', {
+        comment: this.comment,
+        type: 'Point',
+        args: {
+          layer: this.layerObj.layer
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style></style>
