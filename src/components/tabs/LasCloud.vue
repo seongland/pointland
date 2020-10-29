b21b46b2609419b6e2c06e1f9f692d85d6136253
seonglae <sungle3737@gmail.com>
2020-10-29 14:09:05 +0900

    remove tool bar

--- src/components/tabs/LasCloud.vue
+++ src/components/tabs/LasCloud.vue
@@ -2,11 +2,11 @@
 
 <template>
   <div>
-    <v-toolbar class="pt-3 px-3" color="grey darken-4" dense>
-      <!-- <v-slider label="X" :value="0" :max="1" :min="-1" step="0.005" />
+    <!-- <v-toolbar class="pt-3 px-3" color="grey darken-4" dense>
+      <v-slider label="X" :value="0" :max="1" :min="-1" step="0.005" />
       <v-slider label="Y" :value="0" :max="1" :min="-1" step="0.005" />
-      <v-slider label="Z" :value="0" :max="1" :min="-1" step="0.005" /> -->
-    </v-toolbar>
+      <v-slider label="Z" :value="0" :max="1" :min="-1" step="0.005" />
+    </v-toolbar> -->
     <div id="las" />
   </div>
 </template>
