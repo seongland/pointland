798a09d332bbba3aa04a763bd20b7d9a1d691d03
seonglae <sungle3737@gmail.com>
2020-11-25 13:19:40 +0900

    color help update

--- src/pages/help.vue
+++ src/pages/help.vue
@@ -4,10 +4,10 @@
       <v-col lg="6" md="8" sm="12">
         <v-card class="elevation-24">
           <v-toolbar dark flat>
-            <v-toolbar-title><span>시설물</span> 도화 사용법</v-toolbar-title>
+            <v-toolbar-title style="font-weight: bold">시설물 도화 사용법</v-toolbar-title>
           </v-toolbar>
 
-          <v-card-title>단축키</v-card-title>
+          <v-card-title style="font-weight: bold">단축키</v-card-title>
           <v-divider />
 
           <v-card-subtitle>전역 단축키</v-card-subtitle>
@@ -32,7 +32,7 @@
           <v-card-text><kbd>ctrl + 2</kbd> - Image View로 이동한다</v-card-text>
           <v-card-text><kbd>ctrl + 3</kbd> - Cloud View로 이동한다 </v-card-text>
 
-          <v-card-title>UI </v-card-title>
+          <v-card-title style="font-weight: bold">작업 툴 구조</v-card-title>
           <v-divider />
           <v-card-subtitle>메인 뷰</v-card-subtitle>
           <v-card-text><kbd>지도</kbd> - 탑뷰를 볼 수 있다</v-card-text>
@@ -44,23 +44,34 @@
           <v-card-text><kbd>도구</kbd> - 좌측에 있으며 어떤 시설물을 도화할 지 정한다</v-card-text>
           <v-card-text><kbd>컨트롤 바</kbd> - 우측에 있으며 도화할 대상을 선택하거나 필터링한다</v-card-text>
 
-          <v-card-title>Caution </v-card-title>
+          <v-card-title style="font-weight: bold">작업 레이어</v-card-title>
           <v-divider />
-          <v-card-text>1. 오류 발생 시 캡쳐 후 새로고침을 한다</v-card-text>
-          <v-card-text>2. 이미지에서는 클릭, Cloud에서는 더블클릭으로 포인트를 선택한다</v-card-text>
-          <v-card-text>3. 레이어를 선택해야 포인트를 선택할 수 있다</v-card-text>
-          <v-card-text>4. 현재는 표지판, 신호등 레이어만 도화가 가능하다</v-card-text>
-          <v-card-text>5. 클라우드 뷰가 느려진 경우 브라우저를 종료했다가 새로 킨다</v-card-text>
+          <v-card-subtitle>시설물</v-card-subtitle>
+          <v-card-text><kbd style="background: #990000">붉은색</kbd> - 선택한 포인트</v-card-text>
+          <v-card-text><kbd style="background: #f07a7a">주황색</kbd> - 현재 선택 및 연관 시설물</v-card-text>
+          <v-card-text><kbd style="background: #9911ff">보라색</kbd> - 현재 보이는 시설물들</v-card-text>
+          <v-card-text><kbd style="background: #129">곤색</kbd> - 참조 시설물</v-card-text>
 
-          <v-card-title>예시</v-card-title>
+          <v-card-subtitle>마크</v-card-subtitle>
+          <v-card-text><kbd style="background: #18f">파란색</kbd> - 현재 위치</v-card-text>
+          <v-card-text><kbd style="background: #22dd88">초록색</kbd> - 현재 스냅 마크들</v-card-text>
+          <v-card-text><kbd style="background: #cc0">노란색</kbd> - 이동 가능 마크들</v-card-text>
+          <v-card-text><kbd style="background: #e19">분홍색</kbd> - 총 마크들</v-card-text>
+
+          <v-card-title style="font-weight: bold">주의사항</v-card-title>
           <v-divider />
-          <v-card-text><kbd>전체</kbd></v-card-text>
+          <v-card-text><kbd>오류 발생</kbd> - 캡쳐 후 새로고침하고 Telegram 에 업로드</v-card-text>
+          <v-card-text><kbd>포인트 선택</kbd> - 이미지에서는 클릭, Cloud에서는 더블클릭으로 포인트를 선택</v-card-text>
+
+          <v-card-title style="font-weight: bold">참고자료</v-card-title>
+          <v-divider />
+          <v-card-text><kbd>일람표</kbd></v-card-text>
           <v-img src="/example/total.jpg" />
-          <v-card-text><kbd>표지판</kbd></v-card-text>
+          <v-card-text><kbd>B1</kbd> - 신호등</v-card-text>
           <v-img src="/example/B1_SAFETYSIGN_1.png" />
           <v-img src="/example/B1_SAFETYSIGN_2.png" />
           <v-img src="/example/B1_SAFETYSIGN_3.png" />
-          <v-card-text><kbd>신호등</kbd></v-card-text>
+          <v-card-text><kbd>C1</kbd> - 표지판</v-card-text>
           <v-img src="/example/C1_TRAFFICLIGHT_1.png" />
 
           <v-card-actions>
