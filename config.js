a21b1a522ad8a271cb99968c9e407db095a21f5c
seonglae <sungle3737@gmail.com>
2020-12-03 15:19:47 +0900

    map first commit

---
+++ config.js
@@ -0,0 +1,17 @@
+/**
+ * @summary - Map Constant Module
+ */
+
+// Map
+export const BLUE = '#35C',
+  RED = '#F36',
+  WHITE = '#FFF',
+  YELLOW = '#FE1'
+export const ZINDEX_OL = 10
+export const ZINDEX_PVR = ZINDEX_OL + 5
+export const DFT_RADIUS = 3
+export const DFT_WIDTH = 2
+// Geoserver
+export const ZOOM_DURATION = 400
+export const START_ZOOM = 17
+export const NAVER_ID = 'naver'
