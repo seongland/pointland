f6ee4436cdaf290416ec6da302a0fecc02876e7b
seonglae <sungle3737@gmail.com>
2020-12-09 17:54:07 +0900

    getroot

--- src/server/api/addon/tool/round.js
+++ src/server/api/addon/tool/round.js
@@ -1,5 +1,9 @@
-export function getRootByRound(round) {
-  if (round === 'imms_20201106_220611')
-  return `/mnt/10.2.0.108/mms_test2/2020_imms/00_proj_hdmap/HDMAP_201110_Morai_Sejong/02_output/${round}`
-  return `/mnt/10.2.0.108/mms_test2/2020_imms/00_proj_hdmap/01_cto_output/Daejeon_KAIST/${round}`
+import { ref } from '../../app'
+
+export async function getRoundWithRoot(round) {
+  const app = ref.app
+  const service = app.service('rounds')
+  const roundObj = await service.Model.findOne({ name: round })
+  roundObj.root = `/mnt/${roundObj.nas.ip}/${root}`
+  return roundObj
 }
