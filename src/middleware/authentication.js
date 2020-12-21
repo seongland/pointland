73dc1b19dc744d04b559751610ffef4a9af35f3d
seonglae <sungle3737@gmail.com>
2020-12-21 23:48:09 +0900

    rm config

--- src/middleware/authentication.js
+++
@@ -1,4 +0,0 @@
-export default function({ store, route, _, redirect }) {
-  if (!store.state.ls.accessToken && route.name !== 'login' && route.name !== 'signup') return redirect('/login')
-  else if (store.state.ls.accessToken && (route.name === 'login' || route.name === 'signup')) return redirect(`/`)
-}
--- src/plugins/init/index.js
+++
@@ -1,6 +0,0 @@
-import Vue from 'vue'
-import { initCloud } from '~/modules/cloud/init'
-
-export default () => {
-  Vue.mixin({ methods: { initCloud: option => initCloud(option) } })
-}
