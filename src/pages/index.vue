afefe88fd59f4cbff075c69697e068941bfa52dd
seonglae <sungle3737@gmail.com>
2021-03-17 17:49:21 +0900

    Pointland Usage

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="wrapper">
     <las-cloud class="wrapper" />
+    <help-btn />
     <v-overlay :value="$store.state.loading"> <v-progress-circular indeterminate size="64"></v-progress-circular></v-overlay>
   </div>
 </template>
 
 <script>
 import LasCloud from '~/components/LasCloud'
+import HelpBtn from '~/components/HelpBtn'
 
 export default {
   components: {
-    LasCloud
+    LasCloud,
+    HelpBtn
   },
   fetchOnServer: false,
 
