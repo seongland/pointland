50abfb8c4c1656d0cb42c80c56b8b44e899b9560
seonglae <sungle3737@gmail.com>
2021-03-24 17:23:09 +0900

    🏍pointland visit cloudflare analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,7 +47,7 @@ module.exports = {
     transpile: ['three'],
     extend() {}
   },
-  
+
   // Render
   render: {
     csp: {
@@ -58,6 +58,13 @@ module.exports = {
   // Header
   head: {
     title: process.env.title,
+    script: [
+      {
+        src: 'https://static.cloudflareinsights.com/beacon.min.js',
+        defer: true,
+        ['data-cf-beacon']: '{"token": "8f27ea7e450344a5a9340d4cc3b3f1dd"}'
+      }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
