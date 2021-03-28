f001ff4057573c2c05e6b86ec0fd6060b9a71843
seonglae <sungle3737@gmail.com>
2021-03-29 01:51:03 +0900

    gtag init

--- src/plugins/init/index.js
+++ src/plugins/init/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import { initCloud } from '~/modules/cloud/init'
+import VueGtag from 'vue-gtag'
 
 export default () => {
+  Vue.use(VueGtag, {
+    config: { id: 'G-G46T45L2RZ' }
+  })
   Vue.mixin({ methods: { initCloud: option => initCloud(option) } })
 }
