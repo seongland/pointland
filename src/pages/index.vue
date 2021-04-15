e6ce73ca5896db13c443c6d19aa71d08a97bd6e9
seonglae <sungle3737@gmail.com>
2021-04-15 18:08:31 +0900

    ssr complete

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="wrapper">
-    <point-land class="wrapper" />
+    <client-only>
+      <point-land class="wrapper" />
+    </client-only>
     <help-btn />
     <v-overlay :value="$store.state.loading"> <v-progress-circular indeterminate size="64"></v-progress-circular></v-overlay>
   </div>
