6dff5497960a02ef7b57f0c3adee322188620c88
Restyled.io <commits@restyled.io>
2021-09-08 15:56:00 +0000

    Restyled by prettier

--- src/static/sw.js
+++ src/static/sw.js
@@ -2,11 +2,17 @@
 
 // https://github.com/NekR/self-destroying-sw
 
-self.addEventListener('install', function(e) { self.skipWaiting() })
+self.addEventListener('install', function (e) {
+  self.skipWaiting()
+})
 
-self.addEventListener('activate', function(e) {
-  self.registration.unregister()
-      .then(function() { return self.clients.matchAll() })
-      .then(function(
-          clients) { clients.forEach(client => client.navigate(client.url)) })
+self.addEventListener('activate', function (e) {
+  self.registration
+    .unregister()
+    .then(function () {
+      return self.clients.matchAll()
+    })
+    .then(function (clients) {
+      clients.forEach((client) => client.navigate(client.url))
+    })
 })
