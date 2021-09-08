093513ec4c00d95ee4425aef03857d1f28d00bd1
Restyled.io <commits@restyled.io>
2021-09-08 15:55:59 +0000

    Restyled by clang-format

--- src/static/sw.js
+++ src/static/sw.js
@@ -2,16 +2,11 @@
 
 // https://github.com/NekR/self-destroying-sw
 
-self.addEventListener('install', function (e) {
-  self.skipWaiting()
-})
+self.addEventListener('install', function(e) { self.skipWaiting() })
 
-self.addEventListener('activate', function (e) {
+self.addEventListener('activate', function(e) {
   self.registration.unregister()
-    .then(function () {
-      return self.clients.matchAll()
-    })
-    .then(function (clients) {
-      clients.forEach(client => client.navigate(client.url))
-    })
+      .then(function() { return self.clients.matchAll() })
+      .then(function(
+          clients) { clients.forEach(client => client.navigate(client.url)) })
 })
