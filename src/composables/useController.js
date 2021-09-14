147a6e165e662000de7402243d7c6d34edc1649e
seonglae <sungle3737@gmail.com>
2021-09-15 00:30:34 +0900

    feat: desktop fast interaction nipple

--- src/composables/useController.js
+++ src/composables/useController.js
@@ -1,8 +1,20 @@
 import { reactive, toRefs, ref } from '@vue/composition-api'
 import nipplejs from 'nipplejs'
+import { ElementHold } from 'hold-event'
 
 export default function useController() {
-  const controller = reactive({})
+  const mouse = reactive({ x: 0, y: 0 })
+  document.addEventListener(
+    'mousemove',
+    (e) => {
+      mouse.x = e.clientX
+      mouse.y = e.clientY
+    },
+    false
+  )
+  const dirControl = reactive({ force: 0, vector: { x: 0, y: 0 } })
+  const xyControl = reactive({ force: 0, vector: { x: 0, y: 0 } })
+  const zControl = reactive({ force: 0, vector: { x: 0, y: 0 } })
   const touchable = ref(true)
 
   function checkTouchable(space) {
@@ -16,56 +28,76 @@ export default function useController() {
 
   function nippleEvent(manager, space) {
     manager.on('added', (_, nipple) => {
-      if (nipple.position.y < window.innerHeight / 2) verticalNipple(nipple, space)
-      else if (nipple.position.x < window.innerWidth / 2) cameraNipple(nipple, space)
-      else targetNipple(nipple, space)
+      if (nipple.position.y < window.innerHeight / 2) zNipple(nipple, space)
+      else if (nipple.position.x < window.innerWidth / 2) xyNipple(nipple, space)
+      else dirNipple(nipple, space)
     })
   }
 
-  function cameraNipple(nipple, space) {
-    let loop
-    let factor = 1
-    nipple.on('move', (_, data) => {
-      if (loop) clearInterval(loop)
-      loop = setInterval(() => {
-        space.controls.truck((data.force * data.vector.x) / 10, 0, false)
-        space.controls.forward((data.force * data.vector.y) / 10, false)
-        data.vector.x /= factor
-        data.vector.y /= factor
-      }, 10)
+  function dirNipple(nipple, space) {
+    const holder = new ElementHold(nipple.el, 10)
+    nipple.el.addEventListener('mouseover', (e) => holder._holdStart(e), { once: true })
+    holder.addEventListener('holding', (event) => {
+      space.controls.rotate(
+        (-(dirControl.force * dirControl.vector.x) * event?.deltaTime) / 1000,
+        (dirControl.force * dirControl.vector.y * event?.deltaTime) / 1000,
+        true
+      )
     })
-    nipple.on('end', () => (factor = 1.1))
-    nipple.on('destroyed', () => loop && clearInterval(loop))
+    nipple.on('move', (_, data) => {
+      dirControl.force = data.force
+      dirControl.vector = data.vector
+    })
+    nipple.on('destroyed', () => {
+      holder._holdEnd()
+      dirControl.force = 0
+      dirControl.vector = { x: 0, y: 0 }
+    })
   }
 
-  function verticalNipple(nipple, space) {
-    let loop
-    let factor = 1
-    nipple.on('move', (_, data) => {
-      if (loop) clearInterval(loop)
-      loop = setInterval(() => {
-        space.controls.truck(0, -(data.force * data.vector.y) / 10, false)
-        data.vector.x /= factor
-        data.vector.y /= factor
-      }, 10)
+  function zNipple(nipple, space) {
+    const holder = new ElementHold(nipple.el, 10)
+    nipple.el.addEventListener('mouseover', (e) => holder._holdStart(e), { once: true })
+    holder.addEventListener('holding', (event) => {
+      space.controls.truck(0, -((zControl.force * zControl.vector.y) / 50) * event?.deltaTime, true)
+      nipple.el.style.left = `${mouse.x}px`
+      nipple.el.style.top = `${mouse.y}px`
     })
-    nipple.on('end', () => (factor = 1.1))
-    nipple.on('destroyed', () => loop && clearInterval(loop))
+    nipple.on('move', (_, data) => {
+      zControl.force = data.force
+      zControl.vector = data.vector
+    })
+    nipple.on('destroyed', () => {
+      holder._holdEnd()
+      zControl.force = 0
+      zControl.vector = { x: 0, y: 0 }
+    })
   }
-  function targetNipple(nipple, space) {
-    let loop
-    let factor = 1
-    nipple.on('move', (_, data) => {
-      if (loop) clearInterval(loop)
-      loop = setInterval(() => {
-        space.controls.rotate(-(data.force * data.vector.x) / 200, (data.force * data.vector.y) / 200, false)
-        data.vector.x /= factor
-        data.vector.y /= factor
-      }, 10)
+  function xyNipple(nipple, space) {
+    const holder = new ElementHold(nipple.el, 10)
+    nipple.el.addEventListener('mouseover', (e) => holder._holdStart(e), { once: true })
+    holder.addEventListener('holding', (event) => {
+      space.controls.truck(((xyControl.force * xyControl.vector.x) / 50) * event?.deltaTime, 0, true)
+      space.controls.forward(((xyControl.force * xyControl.vector.y) / 50) * event?.deltaTime, true)
+      nipple.el.style.left = `${mouse.x}px`
+      nipple.el.style.top = `${mouse.y}px`
     })
-    nipple.on('end', () => (factor = 1.1))
-    nipple.on('destroyed', () => loop && clearInterval(loop))
+    nipple.on('move', (_, data) => {
+      xyControl.force = data.force
+      xyControl.vector = data.vector
+    })
+    nipple.on('destroyed', () => {
+      holder._holdEnd()
+      xyControl.force = 0
+      xyControl.vector = { x: 0, y: 0 }
+    })
   }
 
-  return { checkTouchable, controller: toRefs(controller), touchable }
+  return {
+    checkTouchable,
+    direction: toRefs(dirControl),
+    xy: toRefs(xyControl),
+    z: toRefs(zControl),
+    touchable,
+  }
 }
