bdbb4b46d4f3db06c13683880803b975caaae78e
Seonglae Cho <sungle3737@gmail.com>
2025-06-06 16:19:21 +0100

    migrate to pnpm and replace nuxt with vite

--- src/store/index.js
+++ src/store/index.js
@@ -1,29 +1,27 @@
-export const state = () => ({
-  loading: false,
-  snackbar: { messages: [] }
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    loading: false,
+    snackbar: { messages: [] },
+  },
+  mutations: {
+    setLoading(state, value) {
+      state.loading = value
+    },
+    snack(state, { message = 'Success', color = 'darkgrey', timeout = 2000 }) {
+      const msgObj = { message, color, timeout }
+      state.snackbar.open = true
+      state.snackbar.messages.push(msgObj)
+    },
+    setState(state, { props, value }) {
+      let target = state
+      for (const i in props)
+        if (Number(i) === props.length - 1) target[props[i]] = value
+        else target = target[props[i]]
+    },
+  },
 })
-
-export const mutations = {
-  setLoading(state, value) {
-    /**
-     * @summary - set global loading state
-     */
-    state.loading = value
-  },
-
-  snack(state, { message = 'Success', color = 'darkgrey', timeout = 2000 }) {
-    /**
-     * @summary - send messages to snackbar and log
-     */
-    const msgObj = { message, color, timeout }
-    state.snackbar.open = true
-    state.snackbar.messages.push(msgObj)
-  },
-
-  setState(state, { props, value }) {
-    let target = state
-    for (const i in props)
-      if (Number(i) === props.length - 1) target[props[i]] = value
-      else target = target[props[i]]
-  }
-}
