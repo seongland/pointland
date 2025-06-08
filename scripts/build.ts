19163e7984908428f40b3f76d931b754db7b50bf
seonglae <sungle3737@gmail.com>
2025-06-08 01:40:04 +0100

    refactor: nuxt to vite static directory without build

--- scripts/build.ts
+++
@@ -1,9 +0,0 @@
-import generateFavicons from './generate-favicons'
-
-async function build() {
-  const promises = []
-  promises.push(await generateFavicons())
-  await Promise.all(promises)
-}
-
-build()
