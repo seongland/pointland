3ac13f3f5007de431c06d10960944d3f13380a9d
seonglae <sungle3737@gmail.com>
2025-12-30 23:40:37 +0000

    fix(lint): use CommonJS format for eslint.config.cjs
    - Convert eslint.config.js to eslint.config.cjs with CommonJS syntax
    - Remove "type": "module" from package.json
    - ESLint v9 supports both ES modules and CommonJS for flat config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,12 +1,12 @@
-import js from '@eslint/js'
-import tseslint from '@typescript-eslint/eslint-plugin'
-import tsparser from '@typescript-eslint/parser'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import prettier from 'eslint-plugin-prettier'
-import prettierConfig from 'eslint-config-prettier'
+const js = require('@eslint/js')
+const tseslint = require('@typescript-eslint/eslint-plugin')
+const tsparser = require('@typescript-eslint/parser')
+const react = require('eslint-plugin-react')
+const reactHooks = require('eslint-plugin-react-hooks')
+const prettier = require('eslint-plugin-prettier')
+const prettierConfig = require('eslint-config-prettier')
 
-export default [
+module.exports = [
   {
     ignores: [
       'node_modules/',
